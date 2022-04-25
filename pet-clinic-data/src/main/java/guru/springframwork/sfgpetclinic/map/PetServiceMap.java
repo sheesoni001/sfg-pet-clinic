d059f13323fc46f128061cc3bb697a5be0432f1f
sheetal k soni <sheetal.k.soni@gmail.com>
2022-04-25 14:09:14 +0530

    impl mapbased services

---
+++ pet-clinic-data/src/main/java/guru/springframwork/sfgpetclinic/map/PetServiceMap.java
@@ -0,0 +1,36 @@
+package guru.springframwork.sfgpetclinic.map;
+
+import guru.springframwork.sfgpetclinic.model.Pet;
+import guru.springframwork.sfgpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet,Long>
+                            implements CrudService<Pet,Long>
+{
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(),object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Pet object) {
+            super.delete(object);
+    }
+
+}
--- pet-clinic-data/src/main/java/guru/springframwork/sfgpetclinic/services/CrudService.java
+++ pet-clinic-data/src/main/java/guru/springframwork/sfgpetclinic/services/CrudService.java
@@ -3,7 +3,7 @@ package guru.springframwork.sfgpetclinic.services;
 import java.util.Set;
 
 public interface CrudService<T,ID> {
-    Set<T> finall();
+    Set<T> findAll();
     T save(T object);
     T findById(ID id);
     void delete(T object);
